f89ef92ea34b133508df7aaf00794e30cdadbfb9
GFlyan <guilhermeflyan.22@gmail.com>
2025-04-25 20:36:10 -0300

    Adding Completed Exercises
    I've added all C exercises I've completed.

--- Ex259/main.c
+++ Ex259/main.c
@@ -1,11 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define hashKey int //Chave hash
+#define amountHashKeys int //Quantidade de chaves que serão tratadas pela tabela hash
+#define hashSize (2 * amountHashKeys) //Bom tamanho definido para tabela hash, onde pegamos o valor primo mais próximo
+#define hashFunction(key) (key % hashSize) //Função espalhamento/hash
+#define factor (amountHashKeys / hashSize) //Diz respeito a porcentagem de preenchimento da tabela hash, quaõ mais próximo de 1/2 melhor
+#define maxColisions int //É importante definir o máximo de colisões de inserção e busca na tabela hash
 
 /*Exercício 259: Tabela Hash
 
 Uma tabela hash tem a mesma funcionalidade das estruturas de dados
 convencionais, armazenar dados, entretanto a principal finalidade
 de uma tabela hash é proporcionar uma busca de forma eficiente.
+
+Os conjunto de dados analisados por uma tabela hash tendem a não sofrer
+muitas alterações (inserções remoções) pois é necessário definir um tamanho
+para a table hash que é "complicado" de se obter.
+
+As chave hashs são únicas, ou seja, não pode haver chaves hashs repetidas,
+exceto para tabelas hash implementadas com listas encadeadas.
 
 CONCEITOS IMPORTANTES:
 
@@ -15,23 +28,27 @@ FUNCAO HASH/ESPALHAMENTO ->  Critério de organização das chaves na tabela has
 gera também o índice de acesso de uma chave na tabela.
 
 TABELA HASH -> Array que armazena as chaves dispostas de acorco com o critério
-da função hash.
+da função hash, pode ser dos tipos: ENDEREÇAMENTO ABERTO/LINEAR, DOUBLE HASHING
+ou ARRARY DE LISTAS ENCADEADAS
 
 TAMANHO DA TABELA HASH -> Quantidade máxima de elementos na tabela, dada pela
 formula 2*quantidadeElementos, aproximando-se do número primo mais próximo.
 
-FATOR DE CARGA -> Definido pela quantidadeElementos/tamanhoTabela, define a
+FATOR DE CARGA -> Definido pela quantidadeChaves/tamanhoTabela, define a
 porcentagem de preenchimento da tabela, é um valor entre 0 e 1, sendo que
-quanto mais proximo de 0.5, mais eficiente será a tabela.
+quanto mais proximo de 0.5, mais eficiente será a tabela, conceito importante
+principalmente para implementação utilizando ENDEREÇAMENTO ABERTO.
 
 COLISOES -> Ocorre quando há mais de um resultado apos a aplicacao da funcao hash.
 
 TRATAMENTO DE COLISOES
 
-TABELA HASH COM ARRAY SOMENTE ESTÁTICO -> A chave será guardada na próxima posição vazia
-do array.
+ENDEREÇAMENTO ABERTO/LINEAR-> A chave será guardada na próxima posição vazia
+do array, importante a utilização de um número máximo de colisões.
 
-TABELA HASH COM ARRAY ESTÁTICO E LISTA ENCADEADA -> As posições do array apontam
+DOUBLE HASHING->
+
+TABELA HASH COM ARRAY DE LISTA ENCADEADA -> As posições do array apontam
 para as chaves, sendo que cada chave, se houver colisão em dada posição, apontará
 para outra chave.
 
@@ -39,61 +56,9 @@ para outra chave.
 
 //TABELA HASH SOMENTE COM ARRAY ESTÁTICO
 
-typedef struct {
-    int id, value;
-} Key;
-
-void insertArrayKey(Key *arrayKey, int sizeArrayKey) {
-    for(int i = 0; i < sizeArrayKey; i++) {
-        printf("---------------------------------------------------\n");
-        printf("INSIRA A %dª CHAVE : ", i+1);
-        scanf("%d", &arrayKey[i].id);
-        printf("INSIRA O %dº VALOR: ", i+1);
-        scanf("%d", &arrayKey[i].value);
-        printf("---------------------------------------------------\n");
-    }
-}
-
-void printKeyArray(Key *arrayKey, int sizeArray) {
-    for(int i = 0; i < sizeArray; i++) {
-        printf("ID KEY: %d\n", arrayKey[i].id);
-        printf("VALUE KEY: %d\n", arrayKey[i].value);
-    }
-}
 
 int main()
 {
-    int option;
-    do {
-        printf("---------------------------------------------------\n");
-        printf("1 - TABELA HASH SOMENTE COM ARRAY ESTATICO\n2 - TABELA HASH COM ARRAY ESTATICO E LISTA ENCADEADA\n0 - SAIR\n");
-        printf("---------------------------------------------------\n");
-        scanf("%d", &option);
-        switch(option){
-
-            case 1:
-
-                printf("---------------------------------------------------\n");
-                int sizeArrayKeyStatic;
-                printf("INFORME A QUANTIDADE DE CHAVES: ");
-                scanf("%d", &sizeArrayKeyStatic);
-                Key *arrayKeyStatic = malloc(sizeof(int)*sizeArrayKeyStatic);
-                insertArrayKey(arrayKeyStatic, sizeArrayKeyStatic);
-                printKeyArray(arrayKeyStatic, sizeArrayKeyStatic);
-                break;
-
-            default:
-                if(option) {
-                    printf("---------------------------------------------------\n");
-                    printf("OPCAO INVALIDA\n");
-                    printf("---------------------------------------------------\n");
-                } else {
-                    printf("---------------------------------------------------\n");
-                    printf("FINALIZANDO PROGRAMA\n");
-                    printf("---------------------------------------------------\n");
-                }
-                break;
-        }
-    } while(option);
+    printf(":)\n");
     return 0;
 }
